c9fd18ba7021804c20d5ca210dc2fbdb8b8162ef
lpdft <772346730@qq.com>
2020-08-18 13:10:29 +0800

    [ruyue] feat:add button jump to ImageListActivity

--- app/src/main/java/com/ruyue/recyclerview/MainActivity.java
+++ app/src/main/java/com/ruyue/recyclerview/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button jumpToRecyclerViewBtn = findViewById(R.id.jump_to_recyclerView);
         Button jumpToMultipleBtn = findViewById(R.id.jump_to_multiple);
+        Button jumpToImgBtn = findViewById(R.id.jump_to_image);
 
         jumpToMultipleBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,10 +25,19 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         jumpToRecyclerViewBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        jumpToImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ImageListActivity.class);
                 startActivity(intent);
             }
         });
